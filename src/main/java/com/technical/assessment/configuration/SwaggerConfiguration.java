8100932a603f355da493fb8046a98efba2d13306
SantiagoKirylukMaclean <santiago.kiryluk@gmail.com>
2020-04-01 12:02:43 +0200

    Code Clean SwaggerConfiguration

--- src/main/java/com/technical/assessment/configuration/SwaggerConfiguration.java
+++ src/main/java/com/technical/assessment/configuration/SwaggerConfiguration.java
@@ -1,37 +1,48 @@
 package com.technical.assessment.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHORIZATION;
 import static springfox.documentation.builders.PathSelectors.regex;
-
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    @Value("${swagger.basePackage}")
+    private String swaggerBasePackage;
+
+    @Value("${swagger.regex}")
+    private String swaggerRegex;
+
+    @Value("${swagger.apiKey.name}")
+    private String swaggerApiKeyName;
+
+    @Value("${swagger.apiKey.keyname}")
+    private String swaggerApiKeyKeyname;
+
+    @Value("${swagger.apiKey.passAs}")
+    private String swaggerApiPassAs;
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.technical.assessment.controller"))
-                .paths(regex("/.*"))
+                .apis(RequestHandlerSelectors.basePackage(swaggerBasePackage))
+                .paths(regex(swaggerRegex))
                 .build()
                 .securitySchemes(Collections.singletonList(apiKey()));
     }
     private ApiKey apiKey() {
-        return new ApiKey("Authorization", "Bearer", "header");
+        return new ApiKey(swaggerApiKeyName, swaggerApiKeyKeyname, swaggerApiPassAs);
     }
-
-
 
 }
