c315fb3acfcbdebfbbe2b48b0ff560abfe6249a9
SantiagoKirylukMaclean <santiago.kiryluk@gmail.com>
2020-04-01 11:33:09 +0200

    Code clean & Refactoring ChallengeApplication y SecurityConfiguration

--- src/main/java/com/technical/assessment/ChallengeApplication.java
+++ src/main/java/com/technical/assessment/ChallengeApplication.java
@@ -1,5 +1,6 @@
 package com.technical.assessment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,14 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ChallengeApplication implements WebMvcConfigurer {
 
+	@Value("${swagger.ui}")
+	private String swaggerUi;
+
+	@Value("${app.resources}")
+	private String appResources;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html")
-				.addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler(swaggerUi)
+				.addResourceLocations(appResources);
 	}
 
 }
--- src/main/java/com/technical/assessment/configuration/SecurityConfiguration.java
+++ src/main/java/com/technical/assessment/configuration/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.technical.assessment.security.jwt.JwtAuthTokenFilter;
 import com.technical.assessment.security.service.UserDetailsServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,18 +18,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
     @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     private JwtAuthEntryPoint unauthorizedHandler;
@@ -62,11 +57,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
 
-        http.cors().and().csrf().disable().
-                authorizeRequests()
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/api/v1.0/auth/**").permitAll()
-                .antMatchers("/api/v1.0/user/**").permitAll()
-                .antMatchers("/api/v1.0/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -74,7 +67,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
     }
 
     @Override
@@ -88,10 +80,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/swagger-ui.html",
                         "/webjars/**");
     }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
 
 }
